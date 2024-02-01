ea070505b54703b3f5736cf817d0b8218befcc07
aagneye-syam <saagneye2003@gmail.com>
2024-02-01 15:23:47 +0530

    npm i @mui/icons-material

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
   return (
