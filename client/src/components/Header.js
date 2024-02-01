ff24f9776773f914746548661eee398745e3c6d1
aagneye-syam <saagneye2003@gmail.com>
2024-02-01 15:19:31 +0530

    AppBar position made static

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 
 function Header() {
   return (
     <div>
-      
+      <AppBar position='static'>
+        <Toolbar>
+
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
