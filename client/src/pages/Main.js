16a330315470863f2f3be68517867ab8e3f0ebf5
aagneye-syam <saagneye2003@gmail.com>
2024-02-01 15:11:43 +0530

    Main.js added

---
+++ client/src/pages/Main.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <div>Display Mail</div>
+    </div>
+  );
+};
+
+export default Main;
