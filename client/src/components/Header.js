d8d195cfc247403c8523535e730a09eedc2ff51b
aagneye-syam <saagneye2003@gmail.com>
2024-02-04 22:59:08 +0530

    gmailLogo added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import styled from '@emotion/styled';
 import MenuIcon from '@mui/icons-material/Menu';
+import { gmailLogo } from '../constants/constant';
 
 const StyledAppBar = styled(AppBar)({
   background: '#F5F5F5',
@@ -15,6 +16,7 @@ function Header() {
       <StyledAppBar position='static'>
         <Toolbar>
          <MenuIcon color='action'/>
+         <img src={gmailLogo} alt='logo' style={{ width:110, height:30, marginLeft:15}}/>
         </Toolbar>
       </StyledAppBar>
     </div>
---
+++ client/src/constants/constant.js
@@ -0,0 +1,1 @@
+export const gmailLogo = 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Gmail2020.logo.png'
