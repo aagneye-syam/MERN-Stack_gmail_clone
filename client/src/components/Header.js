a917cc8abee5daa86b1576d3fd2bb0b54c8743fd
aagneye-syam <saagneye2003@gmail.com>
2024-02-04 23:06:22 +0530

    SearchIcon, InputBase,TuneIcon added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,9 @@ import Toolbar from '@mui/material/Toolbar';
 import styled from '@emotion/styled';
 import MenuIcon from '@mui/icons-material/Menu';
 import { gmailLogo } from '../constants/constant';
+import SearchIcon from '@mui/icons-material/Search';
+import TuneIcon from '@mui/icons-material/Tune';
+import { InputBase } from '@mui/material';
 
 const StyledAppBar = styled(AppBar)({
   background: '#F5F5F5',
@@ -18,6 +21,11 @@ function Header() {
          <MenuIcon color='action'/>
          <img src={gmailLogo} alt='logo' style={{ width:110, height:30, marginLeft:15}}/>
         </Toolbar>
+        <div>
+          <SearchIcon color='action'/>
+          <InputBase />
+          <TuneIcon />
+        </div>
       </StyledAppBar>
     </div>
   )
