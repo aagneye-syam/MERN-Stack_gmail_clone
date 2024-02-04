45b534f1eeedc0af33f217a9132ef846f57d5383
aagneye-syam <saagneye2003@gmail.com>
2024-02-04 22:45:56 +0530

    AppBar is styled

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import styled from '@emotion/styled';
 import MenuIcon from '@mui/icons-material/Menu';
+
+const StyledAppBar = styled(AppBar)({
+  background: '#F5F5F5'
+})
 
 function Header() {
   return (
     <div>
-      <AppBar>
+      <StyledAppBar position='static'>
         <Toolbar>
          <MenuIcon/>
         </Toolbar>
-      </AppBar>
+      </StyledAppBar>
     </div>
   )
 }
