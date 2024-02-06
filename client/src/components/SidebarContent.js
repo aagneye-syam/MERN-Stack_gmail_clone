4dc0e098aff3070ee793e476ffdd09e4c783068e
aagneye-syam <saagneye2003@gmail.com>
2024-02-06 15:46:51 +0530

    SidebarContent addde by map()

--- client/src/components/SidebarContent.js
+++ client/src/components/SidebarContent.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { Box, Button,styled } from "@mui/material";
+import { Box, Button, styled, List, ListItem } from "@mui/material";
 import CreateOutlined from "@mui/icons-material/Create";
 import SIDEBAR_DATA from "../config/Sidebar.config";
 
 const ComposeButton = styled(Button)({
-        background:'#c2e7ff',
-        color: '#001d35',
-        padding: 16,
-        borderRadius:16,
-        minWidth: 140,
-        textTransform:'none'
-})
+  background: "#c2e7ff",
+  color: "#001d35",
+  padding: 16,
+  borderRadius: 16,
+  minWidth: 140,
+  textTransform: "none",
+});
 
 function SidebarContent() {
   return (
     <div>
       <Box>
         <ComposeButton>
-        <CreateOutlined style={{paddingRight:'10px'}} />
-           Compose
+          <CreateOutlined style={{ paddingRight: "10px" }} />
+          Compose
         </ComposeButton>
-
+        <List>
+          {SIDEBAR_DATA.map((data) => (
+            <ListItem>
+                <data.icon fontSize="small"/>{data.title}</ListItem>
+          ))}
+        </List>
       </Box>
     </div>
   );
--- client/src/config/Sidebar.config.js
+++ client/src/config/Sidebar.config.js
@@ -1,1 +1,47 @@
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import StarOutlineIcon from "@mui/icons-material/StarOutline";
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import SendIcon from '@mui/icons-material/Send';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
+const SIDEBAR_DATA = [
+  {
+    name: "inbox",
+    title: "Inbox",
+    icon: InsertPhotoIcon,
+  },
+  {
+    name: "starred",
+    title: "Starred",
+    icon: StarOutlineIcon,
+  },
+  {
+    name: "snoozed",
+    title: "Snoozed",
+    icon: AccessTimeIcon,
+  },
+  {
+    name: "send",
+    title: "Send",
+    icon: SendIcon,
+  },
+  {
+    name: "draft",
+    title: "Draft",
+    icon: InsertDriveFileIcon,
+  },
+  {
+    name: "trash",
+    title: "Trash",
+    icon: DeleteOutlineIcon,
+  },
+  {
+    name: "all_mail",
+    title: "All Mail",
+    icon: MailOutlineIcon,
+  }
+];
+
+export default SIDEBAR_DATA;
