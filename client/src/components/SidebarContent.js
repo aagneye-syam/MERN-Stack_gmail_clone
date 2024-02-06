57ed28c9bd056e1a7c1539e00f5a9e585783f8df
aagneye-syam <saagneye2003@gmail.com>
2024-02-06 15:05:03 +0530

    styled Button

--- client/src/components/SidebarContent.js
+++ client/src/components/SidebarContent.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button,styled } from "@mui/material";
 import CreateOutlined from "@mui/icons-material/Create";
+import SIDEBAR_DATA from "../config/Sidebar.config";
+
+const ComposeButton = styled(Button)({
+        background:'#c2e7ff',
+        color: '#001d35',
+        padding: 16,
+        borderRadius:16,
+        minWidth: 140,
+        textTransform:'none'
+})
 
 function SidebarContent() {
   return (
     <div>
       <Box>
-        <Button>
-          <CreateOutlined />
-          Compose
-        </Button>
+        <ComposeButton>
+        <CreateOutlined style={{paddingRight:'10px'}} />
+           Compose
+        </ComposeButton>
+
       </Box>
     </div>
   );
