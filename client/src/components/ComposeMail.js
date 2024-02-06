213c8583cb4e318a4c89a35fdb3ae843edf125a7
aagneye-syam <saagneye2003@gmail.com>
2024-02-06 22:32:16 +0530

    dialog box edited

--- client/src/components/ComposeMail.js
+++ client/src/components/ComposeMail.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Dialog } from '@mui/material';
+import React from "react";
+import { Dialog } from "@mui/material";
 
-
+const dialogStyle = {
+  height: "90%",
+  width: "80%",
+  maxWidth: "100%",
+  maxHeight: "100%",
+  boxShadow: "none",
+  borderRadius: "10px 10px 0 0",
+};
 
 function ComposeMail() {
   return (
     <div>
-     <Dialog open={true}
-     
-     >
+      <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
         Hello
-     </Dialog>
+      </Dialog>
     </div>
-  )
+  );
 }
 
 export default ComposeMail;
