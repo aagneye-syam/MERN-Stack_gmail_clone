f4e77e8479bb3b7f2e08d4776ea6976c86361d2f
aagneye-syam <saagneye2003@gmail.com>
2024-02-09 11:02:06 +0530

    database connected successfully

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Connection = async () => {
+  const DB_URI = `mongodb://saagneye2003:4J3LRIWfiRw8SyrS@ac-cs0bkpn-shard-00-00.qdjztxe.mongodb.net:27017,ac-cs0bkpn-shard-00-01.qdjztxe.mongodb.net:27017,ac-cs0bkpn-shard-00-02.qdjztxe.mongodb.net:27017/?ssl=true&replicaSet=atlas-9ewhro-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(DB_URI, { useNewUrlParser: true });
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Error while connecting to the database:', error.message);
+  }
+};
+
+export default Connection;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import Connection from './database/db.js';
 
 const app = express();
 
 
 const PORT = 8000;
+
+Connection();
 
 app.listen(PORT, ()=> console.log(`sever started on ${PORT}`));
