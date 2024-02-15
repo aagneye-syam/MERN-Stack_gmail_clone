d3938c45578a54d2da32385de5685148fdcdb2e2
aagneye-syam <saagneye2003@gmail.com>
2024-02-15 21:46:35 +0530

    req body saving err corrected

--- server/controller/email-controller.js
+++ server/controller/email-controller.js
@@ -3,10 +3,10 @@ import Email from "../model/email.js";
 
 export const saveSendEmails = (req, res) => {
   try {
-       const email = new Email(request.body);
-       email.save();
+    const email = new Email(req.body);
+    email.save();
 
-       response.status(200).json('email saved sucesssfully');
+    response.status(200).json("email saved successfully");
   } catch (err) {
     res.status(500).json(err.message);
   }
